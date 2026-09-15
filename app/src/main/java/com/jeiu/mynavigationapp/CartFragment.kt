6ad6564c4c62b523273f1f541537b742b52de6dc
star057-ic <starplayer057@gmail.com>
2026-09-15 16:17:28 +0900

    카트(장바구니) Fragement 기능 구현

--- app/src/main/java/com/jeiu/mynavigationapp/CartFragment.kt
+++ app/src/main/java/com/jeiu/mynavigationapp/CartFragment.kt
@@ -1,4 +1,48 @@
 package com.jeiu.mynavigationapp
 
-class CartFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.jeiu.mynavigationapp.databinding.FragmentCartBinding
+
+class CartFragment : Fragment() {
+
+    private var _binding: FragmentCartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val item = arguments?.getString("cart_item") ?: "장바구니가 비어있습니다."
+        binding.txtCartItem.text = "장바구니 항목: $item"
+
+        binding.btnOrder.setOnClickListener {
+            val bundle = Bundle().apply {
+                putString("order_item", item)
+            }
+            findNavController().navigate(
+                R.id.action_cartFragment_to_orderConfirmFragment,
+                bundle
+            )
+        }
+
+        binding.btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
