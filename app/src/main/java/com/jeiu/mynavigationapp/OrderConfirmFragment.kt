6e7dc562c39da6167f18bb7d437b6ebc01f647a8
star057-ic <starplayer057@gmail.com>
2026-09-15 16:22:35 +0900

    주문 확인 기능 구현 안료

--- app/src/main/java/com/jeiu/mynavigationapp/OrderConfirmFragment.kt
+++ app/src/main/java/com/jeiu/mynavigationapp/OrderConfirmFragment.kt
@@ -1,4 +1,38 @@
 package com.jeiu.mynavigationapp
 
-class OrderConfirmFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.jeiu.mynavigationapp.databinding.FragmentOrderConfirmBinding
+
+class OrderConfirmFragment : Fragment() {
+
+    private var _binding: FragmentOrderConfirmBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOrderConfirmBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val orderItem = arguments?.getString("order_item") ?: "주문 항목 없음"
+        binding.txtOrderResult.text = "[$orderItem] 주문이 완료되었습니다."
+
+        binding.btnHome.setOnClickListener {
+            findNavController().popBackStack(R.id.homeFragment, false)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
